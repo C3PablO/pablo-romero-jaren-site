bdb2fcf89c8a1ca2eb0dab3ba81edd98b101fd32
Pablo Romero <pablo.romero@cantemo.com>
2022-04-20 17:24:35 +0200

    image fix

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -36,36 +36,29 @@ const customLoader = ({ src }: { src: string }) => {
   return src;
 };
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style={{ opacity: .5 }}>
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
+// const shimmer = (w: number, h: number) => `
+// <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style={{ opacity: .5 }}>
+//   <defs>
+//     <linearGradient id="g">
+//       <stop stop-color="#333" offset="20%" />
+//       <stop stop-color="#222" offset="50%" />
+//       <stop stop-color="#333" offset="70%" />
+//     </linearGradient>
+//   </defs>
+//   <rect width="${w}" height="${h}" fill="#333" />
+//   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+//   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+// </svg>`;
 
-const toBase64 = (str: string) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str);
+// const toBase64 = (str: string) =>
+//   typeof window === 'undefined'
+//     ? Buffer.from(str).toString('base64')
+//     : window.btoa(str);
 
 const components = {
   img: (props: ImageProps) => (
     // height and width are part of the props, so they get automatically passed here with {...props}
-    <Image
-      {...props}
-      layout="responsive"
-      loading="lazy"
-      placeholder="blur"
-      blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
-      loader={customLoader}
-    />
+    <Image {...props} layout="responsive" loader={customLoader} />
   ),
 };
 
