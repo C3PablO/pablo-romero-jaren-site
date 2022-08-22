5249e88058283edb34c33bf6e6c5c1be33d31e74
Pablo Romero <pablo.romero@cantemo.com>
2022-08-22 17:48:34 +0200

    fix prerendering issue

--- next.config.js
+++ next.config.js
@@ -3,29 +3,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const baseUrl = '';
-
 module.exports = withBundleAnalyzer({
   i18n: {
-    // These are all the locales you want to support in
-    // your application
     locales: ['en', 'es'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'en',
     localeDetection: false,
   },
   images: {
     loader: 'custom',
   },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: baseUrl,
-  env: {
-    baseUrl,
-  },
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -51,77 +51,82 @@ const components = {
   img: ImageComp,
 };
 
-const DisplayPost = (props: IPostProps) => (
-  <>
-    <Main
-      style={{ background: props.background }}
-      meta={
-        <Meta
-          title={props.title}
-          description={props.description}
-          post={{
-            image: props.image,
-            date: props.date,
-            modified_date: props.modified_date,
-          }}
-        />
-      }
-    >
-      <div className="fixed top-0 z-50 w-full">
-        <Navbar>
-          <ul className="flex w-full divide-x">
-            <li className="mr-6 transition ease-in-out hover:scale-110 duration-200 cursor-pointer">
-              <Link href="/">
-                <a className="flex no-underline">
-                  <svg
-                    className="mr-2"
-                    width={24}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 28 28"
-                  >
-                    <g clipPath="url(#a)">
-                      <path d="M28 13H4.28L14.73 1.68 13.27.32.64 14l12.63 13.68 1.46-1.36L4.28 15H28v-2Z" />
-                    </g>
-                    <defs>
-                      <clipPath id="a">
-                        <path d="M0 0h28v28H0z" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  {props.localeMessages[props.locale]['page.work.button.top']}
-                </a>
-              </Link>
-            </li>
-            <li className="w-full truncate">
-              <h1 className="text-xs w-full text-indigo-200 text-xl px-5 truncate">
-                {' '}
-                {props.title}
-              </h1>
-            </li>
-          </ul>
-        </Navbar>
-      </div>
-      <div
-        className={`max-w-screen-md mx-auto  px-3 pt-8 md:px-0 ${props.containerClass}`}
+const DisplayPost = (props: IPostProps) => {
+  if (!props.date) {
+    return '';
+  }
+  return (
+    <>
+      <Main
+        style={{ background: props.background }}
+        meta={
+          <Meta
+            title={props.title}
+            description={props.description}
+            post={{
+              image: props.image,
+              date: props.date,
+              modified_date: props.modified_date,
+            }}
+          />
+        }
       >
-        <Content>
-          <MDXRemote {...props.content} components={components as any} />
-        </Content>
-      </div>
-      <div className="flex justify-center">
-        <Link href="/">
-          <a className="text-xl md:text-2xl px-8 py-5 m-8 rounded-md inline-block text-zinc-100 transition ease-in-out hover:scale-110 duration-200 cursor-pointer bg-indigo-800 hover:bg-indigo-500 hover:no-underline no-underline font-normal">
-            {props.localeMessages[props.locale]['page.work.button.bottom']}
-          </a>
-        </Link>
-      </div>
-      <div className="bg-indigo-800 text-zinc-100 text-center text-sm flex p-4">
-        <p>(ɔ) Pablo Romero Jarén 2022</p>
-      </div>
-    </Main>
-  </>
-);
+        <div className="fixed top-0 z-50 w-full">
+          <Navbar>
+            <ul className="flex w-full divide-x">
+              <li className="mr-6 transition ease-in-out hover:scale-110 duration-200 cursor-pointer">
+                <Link href="/">
+                  <a className="flex no-underline">
+                    <svg
+                      className="mr-2"
+                      width={24}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 28 28"
+                    >
+                      <g clipPath="url(#a)">
+                        <path d="M28 13H4.28L14.73 1.68 13.27.32.64 14l12.63 13.68 1.46-1.36L4.28 15H28v-2Z" />
+                      </g>
+                      <defs>
+                        <clipPath id="a">
+                          <path d="M0 0h28v28H0z" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                    {props.localeMessages[props.locale]['page.work.button.top']}
+                  </a>
+                </Link>
+              </li>
+              <li className="w-full truncate">
+                <h1 className="text-xs w-full text-indigo-200 text-xl px-5 truncate">
+                  {' '}
+                  {props.title}
+                </h1>
+              </li>
+            </ul>
+          </Navbar>
+        </div>
+        <div
+          className={`max-w-screen-md mx-auto  px-3 pt-8 md:px-0 ${props.containerClass}`}
+        >
+          <Content>
+            <MDXRemote {...props.content} components={components as any} />
+          </Content>
+        </div>
+        <div className="flex justify-center">
+          <Link href="/">
+            <a className="text-xl md:text-2xl px-8 py-5 m-8 rounded-md inline-block text-zinc-100 transition ease-in-out hover:scale-110 duration-200 cursor-pointer bg-indigo-800 hover:bg-indigo-500 hover:no-underline no-underline font-normal">
+              {props.localeMessages[props.locale]['page.work.button.bottom']}
+            </a>
+          </Link>
+        </div>
+        <div className="bg-indigo-800 text-zinc-100 text-center text-sm flex p-4">
+          <p>(ɔ) Pablo Romero Jarén 2022</p>
+        </div>
+      </Main>
+    </>
+  );
+};
 
 export const getStaticPaths: GetStaticPaths<IPostUrl> = async (context) => {
   const locales = context.locales || [DEFAULT_LOCALE];
