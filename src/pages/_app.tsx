e0bb7c5ca25847771451534f8f0ad5541f2e6d18
Pablo Romero <pablo.romero@cantemo.com>
2022-08-23 22:51:12 +0200

    remove framer dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { AnimatePresence, domAnimation, LazyMotion, m } from 'framer-motion';
 import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
 
-import { animations } from '../lib/animations';
-
-function MyApp({ Component, pageProps, router }: AppProps) {
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    setLoaded(true);
-  }, []);
-  const startIndex = 2;
-  const animation = animations[startIndex];
-  const exitBefore = false;
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="app-wrap bg-amber-50">
-      <LazyMotion features={domAnimation}>
-        <AnimatePresence exitBeforeEnter={!exitBefore}>
-          <m.div
-            key={router.route.concat(animation.name)}
-            className={loaded ? 'page-wrap' : 'page-wrap first'}
-            initial={loaded ? 'initial' : undefined}
-            animate="animate"
-            exit="exit"
-            variants={animation.variants}
-            transition={animation.transition}
-          >
-            <Component {...pageProps} />
-          </m.div>
-        </AnimatePresence>
-      </LazyMotion>
+    <div className="bg-amber-50">
+      <Component {...pageProps} />
     </div>
   );
 }
