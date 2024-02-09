950bef87dc28259830c11ded573237f2b76d45ec
Pablo Romero <pabloromerojaren@gmail.com>
2024-02-09 04:51:50 +0100

    update images

--- src/components/image_comp.tsx
+++ src/components/image_comp.tsx
@@ -14,7 +14,7 @@ const ImageComp = (props: ImageProps) => {
     // height and width are part of the props, so they get automatically passed here with {...props}
     <span
       style={{ position: 'relative', display: 'block', overflow: 'hidden' }}
-      className="relative block overflow-hidden mt-10 mb-10"
+      className="bg-white relative block overflow-hidden mt-10 mb-10 rounded-lg"
     >
       {loaded ? (
         ''
@@ -28,6 +28,7 @@ const ImageComp = (props: ImageProps) => {
       )}
       <Image
         {...updatedProps}
+        className="relative z-10"
         alt=""
         unoptimized={true}
         loader={customLoader}
