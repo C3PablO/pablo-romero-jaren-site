23bda2b636a5d0945a0294cc28d0649b5d9385ec
Pablo Romero <pabloromerojaren@gmail.com>
2024-02-12 13:49:22 +0100

    update animation

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -16,17 +16,39 @@ type ILocaleProps = {
 };
 const LayoutIndex = (props: IBlogGalleryProps & ILocaleProps) => {
   const [topLoaded, setTopLoaded] = useState(false);
+
+  const [style, setStyle] = useState({
+    opacity: 1,
+    filter: 'blur(0px)', // Initial blur value
+  });
+
+  const handleScroll = () => {
+    // Maximum scroll value at which header is fully transparent
+    const maxScroll = 700; // Adjust this value based on your needs
+    const currentScroll = window.scrollY;
+    const opacity = Math.max(1 - currentScroll / maxScroll, 0);
+    const blur = Math.min((currentScroll / maxScroll) * 2, 10); // Calculate blur from 0 to 10
+
+    setStyle({
+      opacity,
+      filter: `blur(${blur}px)`, // Apply calculated blur value
+    });
+  };
+
   useEffect(() => {
     window.dispatchEvent(new Event('resize'));
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
   return (
     <>
-      <div className="relative z-25">
+      <div className="relative z-25 bg-amber-100">
         <BGLoader
           callback={() => {
             setTopLoaded(true);
           }}
-          className={`custom_header ${
+          className={`sticky top-0 custom_header ${
             topLoaded ? 'custom_header_loaded' : ''
           } relative`}
           placeholderColor={[
@@ -35,6 +57,10 @@ const LayoutIndex = (props: IBlogGalleryProps & ILocaleProps) => {
             '254, 251, 235',
             '254, 251, 235',
           ]}
+          style={{
+            ...style,
+            transition: 'opacity 50ms ease, filter 50ms ease',
+          }}
         >
           <header className="flex flex-col items-center pt-10 text-indigo-900 relative z-50">
             <h1 className="font-display text-vw-5xl min-[600px]:text-[80px] md:text-[110px] text-center leading-none">
@@ -55,84 +81,91 @@ const LayoutIndex = (props: IBlogGalleryProps & ILocaleProps) => {
             </h2>
           </header>
         </BGLoader>
-        <div className="bg-amber-50" id="work">
-          <div className="max-w-screen-md mx-auto px-3 md:px-0">
-            <h2
-              data-aos="fade-up"
-              className="font-display text-vw-5xl min-[600px]:text-[80px] md:text-[110px] md:text-6xl text-center pb-5 pt-5 text-indigo-900"
-            >
-              {props.localeMessages[props.locale]['page.index.title.work']}
-            </h2>
-            <BlogGallery
-              posts={props.posts}
-              pagination={props.pagination}
-              path={props.localeMessages[props.locale]['route.work']}
-            />
-          </div>
-
-          <div className="contact flex flex-col items-center justify-center -mt-[15vh]">
-            <h2
-              data-aos="fade-up"
-              className="font-display text-vw-5xl min-[600px]:text-[80px] md:text-[110px] md:text-6xl text-center py-5 text-indigo-900"
-            >
-              {props.localeMessages[props.locale]['page.index.title.whoami']}
-            </h2>
-          </div>
-          <div
-            className="max-w-screen-md mx-auto px-3 md:px-0 mt-10"
-            data-aos="fade-up"
-          >
-            <p className="text-center pb-5">
-              {props.localeMessages[props.locale]['page.index.text.whoami']}
-            </p>
-            <p className="text-center pb-5">
-              {props.localeMessages[props.locale]['page.index.text.contact']}
-            </p>
-          </div>
-
-          <div className="basedIn flex flex-col items-center justify-start bg-fixed">
+        <div className="relative" id="work">
+          <div className="relative h-24 bg-gradient-to-b from-transparent to-amber-50"></div>
+          <div className="bg-amber-50">
             <div className="max-w-screen-md mx-auto px-3 md:px-0">
               <h2
-                data-aos="flip-up"
+                data-aos="fade-up"
+                className="m-0 font-display text-vw-5xl min-[600px]:text-[80px] md:text-[110px] md:text-6xl text-center pb-5 pt-5 text-indigo-900"
+              >
+                {props.localeMessages[props.locale]['page.index.title.work']}
+              </h2>
+              <BlogGallery
+                posts={props.posts}
+                pagination={props.pagination}
+                path={props.localeMessages[props.locale]['route.work']}
+              />
+            </div>
+
+            <div className="contact flex flex-col items-center justify-center -mt-[15vh]">
+              <h2
+                data-aos="fade-up"
                 className="font-display text-vw-5xl min-[600px]:text-[80px] md:text-[110px] md:text-6xl text-center py-5 text-indigo-900"
               >
-                {props.localeMessages[props.locale]['page.index.title.contact']}
+                {props.localeMessages[props.locale]['page.index.title.whoami']}
               </h2>
             </div>
             <div
+              className="max-w-screen-md mx-auto px-3 md:px-0 mt-10"
               data-aos="fade-up"
-              className="w-80 flex flex-col gap-5 w-72 max-w-full"
             >
-              <Button
-                href="https://www.linkedin.com/in/pabloromerojaren/"
-                markup="a"
-                widthType="w-auto"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Linkedin />
-                <ButtonLabel>LinkedIn</ButtonLabel>
-                <ArrowRight />
-              </Button>
-
-              <Button
-                markup="a"
-                href="https://www.instagram.com/rawromero/"
-                widthType="w-auto"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Instagram />
-                <ButtonLabel>Instagram</ButtonLabel>
-                <ArrowRight />
-              </Button>
+              <p className="text-center pb-5">
+                {props.localeMessages[props.locale]['page.index.text.whoami']}
+              </p>
+              <p className="text-center pb-5">
+                {props.localeMessages[props.locale]['page.index.text.contact']}
+              </p>
             </div>
-          </div>
-          <div className="bg-indigo-900 text-zinc-100 text-center text-sm flex items-center p-4">
-            <a href="#" className="animate-pulse rotate-180">
-              <ArrowDown width={18} height={18} />
-            </a>
-            <p className="w-full">(ɔ) Pablo Romero Jarén 2024</p>
+
+            <div className="basedIn flex flex-col items-center justify-start bg-fixed">
+              <div className="max-w-screen-md mx-auto px-3 md:px-0">
+                <h2
+                  data-aos="fade-up"
+                  className="font-display text-vw-5xl min-[600px]:text-[80px] md:text-[110px] md:text-6xl text-center py-5 text-indigo-900"
+                >
+                  {
+                    props.localeMessages[props.locale][
+                      'page.index.title.contact'
+                    ]
+                  }
+                </h2>
+              </div>
+              <div
+                data-aos="fade-up"
+                className="w-80 flex flex-col gap-5 w-72 max-w-full"
+              >
+                <Button
+                  href="https://www.linkedin.com/in/pabloromerojaren/"
+                  markup="a"
+                  widthType="w-auto"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Linkedin />
+                  <ButtonLabel>LinkedIn</ButtonLabel>
+                  <ArrowRight />
+                </Button>
+
+                <Button
+                  markup="a"
+                  href="https://www.instagram.com/rawromero/"
+                  widthType="w-auto"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Instagram />
+                  <ButtonLabel>Instagram</ButtonLabel>
+                  <ArrowRight />
+                </Button>
+              </div>
+            </div>
+            <div className="bg-indigo-900 text-zinc-100 text-center text-sm flex items-center p-4">
+              <a href="#" className="animate-pulse rotate-180">
+                <ArrowDown width={18} height={18} />
+              </a>
+              <p className="w-full">(ɔ) Pablo Romero Jarén 2024</p>
+            </div>
           </div>
         </div>
       </div>
